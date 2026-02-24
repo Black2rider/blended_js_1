bf99e3719ccf5dfcad23c8efe80345b4a3b797bb
Kostya_Kusik <kostya_kysik@ukr.net>
2026-02-24 21:34:58 +0200

    finish

--- js/lesson-1.js
+++ js/lesson-1.js
@@ -258,16 +258,17 @@
 // якщо ділиться  без остачі і на 3, і на 5 - виводить в консоль 'fizzbuzz'.
 
 
-function fizzBuzz(num) {
-  for (let i = 1; i < num; i++) {
-    if (i % 3 === 0 && i % 5 === 0) {
-      return console.log("fizzbuzz");
-    } if (i % 3 === 0) {
-      return console.log("fizz");
-    } if (i % 5 === 0) {
-      return console.log("buzz");
-    }
-  }
-}
+// function fizzBuzz(num) {
+//   for (let i = 1; i < num; i++) {
+//     if (i % 3 === 0 && i % 5 === 0) {
+//       console.log(`${i} : fizzbuzz`);
+//     } else if (i % 3 === 0) {
+//       console.log(`${i} : fizz`);
+//     } else if (i % 5 === 0) {
+//       console.log(`${i} : buzz`);
+//     }
+//   }
+//   return;
+// }
 
-fizzBuzz(15);
+// fizzBuzz(16);
