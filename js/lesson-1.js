e0e0beb3f25581b838ee709cab41327f271b79a3
Kostya Kusik <60221585+Black2rider@users.noreply.github.com>
2026-02-24 15:54:51 +0200

    8-9

--- js/lesson-1.js
+++ js/lesson-1.js
@@ -212,6 +212,23 @@
 // повертати з функції рядок - 'Not a number!'.
 
 
+// function min(a, b) {
+//    let min = 0;
+//   if (typeof a === "number" && typeof b === "number") {
+//     if (a >= b) {
+//       min = b;
+//     } else { 
+//         min = a;
+//     }
+//   } else {
+//     return "Not a number!"
+//   }
+//   console.log(min);
+// }
+
+// min(4, 5)
+
+
 
 // 9.///////////////////////////////////////////////////////////////
 // Напишіть функцію isAdult(age), яка  приймає число - вік користувача і повертає true, 
@@ -220,9 +237,37 @@
 // і повертає його результат (true/false).
 
 
+// function isAdult(age) {
+//   if (age >= 18) {
+//     return console.log(true);
+//   } else {
+//    return console.log(confirm("Вам не має 18?"));
+//   }
+// }
+
+// isAdult(11);
+
+
+
+
 
 // 10.///////////////////////////////////////////////////////////////
 // Напишіть функцію fizzBuzz(num), яка приймає число і перевіряє кожне число від 1 до num:
 // Якщо число ділитися  без остачі на 3 - виводить в консоль 'fizz',
 // якщо ділиться  без остачі на 5 - виводить в консоль 'buzz',
 // якщо ділиться  без остачі і на 3, і на 5 - виводить в консоль 'fizzbuzz'.
+
+
+function fizzBuzz(num) {
+  for (let i = 1; i < num; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      return console.log("fizzbuzz");
+    } if (i % 3 === 0) {
+      return console.log("fizz");
+    } if (i % 5 === 0) {
+      return console.log("buzz");
+    }
+  }
+}
+
+fizzBuzz(15);
